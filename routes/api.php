48ace09e674db2f3e364a4edcde38412c3e350e6
chakibehappy <debri.pramiarta@gmail.com>
2026-04-26 04:36:15 +0700

    live fix

--- routes/api.php
+++ routes/api.php
@@ -185,29 +185,29 @@ Route::middleware('auth:sanctum')->post('/transactions', function (Request $requ
         // --- START ACTUAL PROCESSING ---
         DB::beginTransaction();
 
-        // Fix for bugs that delete withdrawal cash not adding cash store, due diferent subotal logic and data store
-        // now we follow dashboard, any changes
-        $mainTotal = $request->total;
-        $mainSubTotal = $request->subtotal;
+        // // Fix for bugs that delete withdrawal cash not adding cash store, due diferent subotal logic and data store
+        // // now we follow dashboard, any changes
+        // $mainTotal = $request->total;
+        // $mainSubTotal = $request->subtotal;
 
-        foreach ($request->items as $item) {
-            if (!empty($item['cash_withdrawal'])) {
-                $wdData = $item['cash_withdrawal'];
-                $feeRule = WithdrawalFeeRule::where('min_limit', '<=', $amount)
-                    ->where(function ($q) use ($amount) {
-                        $q->where('max_limit', '>=', $amount)
-                        ->orWhere('max_limit', '<', 0); // unlimited
-                    })
-                    ->orderBy('min_limit', 'desc')
-                    ->first();
+        // foreach ($request->items as $item) {
+        //     if (!empty($item['cash_withdrawal'])) {
+        //         $wdData = $item['cash_withdrawal'];
+        //         $feeRule = WithdrawalFeeRule::where('min_limit', '<=', $amount)
+        //             ->where(function ($q) use ($amount) {
+        //                 $q->where('max_limit', '>=', $amount)
+        //                 ->orWhere('max_limit', '<', 0); // unlimited
+        //             })
+        //             ->orderBy('min_limit', 'desc')
+        //             ->first();
 
-                $adminFee = $feeRule?->fee ?? 0;
-                $amount = $wdData['withdrawal_count'] - adminFee;
+        //         $adminFee = $feeRule?->fee ?? 0;
+        //         $amount = $wdData['withdrawal_count'] - adminFee;
 
-                $mainTotal -= $amount;
-                $mainSubTotal -= $amount;
-            }
-        }
+        //         $mainTotal -= $amount;
+        //         $mainSubTotal -= $amount;
+        //     }
+        // }
 
         // Create Transaction Header
         $transaction = Transaction::create([
