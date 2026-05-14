7188c276a496f48fdc16403ce69b1743950469b1
chakibehappy <debri.pramiarta@gmail.com>
2026-05-15 01:20:36 +0700

    add check shift status

--- routes/api.php
+++ routes/api.php
@@ -710,7 +710,7 @@ Route::middleware('auth:sanctum')->get('/get-store-balance', function (Request $
     // 2. Get Digital Wallet Balances
     // We join with the 'digital_wallets' table to get the wallet names (OVO, GoPay, etc.)
     $digitalWallets = DigitalWalletStore::where('store_id', $storeId)
-        ->join('digital_wallets', 'digital_wallet_stores.digital_wallet_id', '=', 'digital_wallets.id')
+        ->join('digital_wallet', 'digital_wallet_stores.digital_wallet_id', '=', 'digital_wallet.id')
         ->select(
             'digital_wallet_stores.id as store_wallet_id',
             'digital_wallets.name as wallet_name',
