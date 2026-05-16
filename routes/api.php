e7d3ff2551ca730864fbcede529cee2f073a3d42
chakibehappy <debri.pramiarta@gmail.com>
2026-05-16 11:08:45 +0700

    add transaction history api

--- routes/api.php
+++ routes/api.php
@@ -820,7 +820,7 @@ Route::get('/get-detail-transactions/{store_id}', function ($storeId) {
                 'title'         => $title,
                 'desc'          => $desc,
                 'icon'          => $icon,
-                'amount'        => number_format((float)($detail->subtotal ?? 0), 0, ',', '.'), 
+                'amount'        => "Rp ". number_format((float)($detail->subtotal ?? 0), 0, ',', '.'), 
             ]);
         }
     }
